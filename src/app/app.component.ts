a5fb77e9939d71050cc3db84e834f38df09f52bc
artem <artem.sorkin.v@gmail.com>
2024-07-02 05:58:07 +0500

    set initial config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-tasks';
+  title = 'angular-tasks-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TasksComponent,
+    title: 'Tasks App',
+  },
+  // {
+  //   path: 'tasks/1',
+  //   component: Task1Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/2',
+  //   component: Task2Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/3',
+  //   component: Task3Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/4',
+  //   component: Task4Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/5',
+  //   component: Task5Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/6',
+  //   component: Task6Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/7',
+  //   component: Task7Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/8',
+  //   component: Task8Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/9',
+  //   component: Task9Component,
+  //   title: 'Tasks App',
+  // },
+  // {
+  //   path: 'tasks/10',
+  //   component: Task10Component,
+  //   title: 'Tasks App',
+  // },
+];
