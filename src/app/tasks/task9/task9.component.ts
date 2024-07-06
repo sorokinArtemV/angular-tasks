d652dbb01d228c097f4219ca141c9a81b0a1ccde
artem <artem.sorkin.v@gmail.com>
2024-07-06 09:14:39 +0500

    Finish task9: Video background

--- src/app/tasks/task9/task9.component.ts
+++ src/app/tasks/task9/task9.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-task9',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './task9.component.html',
   styleUrl: './task9.component.scss'
 })
-export class Task9Component {
+export class Task9Component implements OnInit {
+  private video = viewChild.required<ElementRef<HTMLVideoElement>>('vid');
+  isPlaying = true;
 
+  ngOnInit(): void {
+    this.video().nativeElement.muted = true;
+  }
+
+  togglePlay() {
+    this.isPlaying = !this.isPlaying;
+    console.log(this.isPlaying);
+    if (this.isPlaying) {
+      this.video().nativeElement.play().catch(err => console.log('video is unavailable'));
+    } else {
+      this.video().nativeElement.pause();
+    }
+  }
 }
